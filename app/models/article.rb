ec04a2ea4d50e703854df031f1caae143169ad43
ammar <ammar@V570c.(none)>
2013-03-29 17:04:29 +0500

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+class Article
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::MultiParameterAttributes
+
+  field :name, type: String
+  field :content, type: String
+  field :published_on, :type => Date
+  validates_presence_of :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '26bf6731a24693b3c0d0d4d101fd295d2861ae0e7bd039bc4963ec558ee7cfe8e7b739c5643f946483b15f763bd353ef63aed6a0e3165f07653c84c145f7d50f'
